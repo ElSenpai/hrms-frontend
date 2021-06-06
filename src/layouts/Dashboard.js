017f0a2af28917a9ada3da2c0678314d32ea90eb
El Senpai <El Senpai@EL-SENPAI>
2021-06-06 20:05:53 +0300

    fix

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -18,6 +18,7 @@ export default function Dashboard() {
             <AdvertisementList/>
             <JobHunterList/>
             <EmployerList/>
+            
           </Grid.Column>
         </Grid.Row>
       </Grid>
